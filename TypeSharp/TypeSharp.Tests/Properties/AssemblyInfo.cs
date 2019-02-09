56510d523eea6d7ee532ef3a6ca77e406e71a8e1
Jacob Larsson <jacob.larsson@canea.se>
2019-02-09 11:35:28 +0100

    First empty sln

---
+++ TypeSharp/TypeSharp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TypeSharp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TypeSharp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5b83ea8e-d2ce-4a09-9637-26c46f20c88c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TypeSharp/TypeSharp/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TypeSharp
+{
+    public class Class1
+    {
+    }
+}
